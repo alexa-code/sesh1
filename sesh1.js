81725d135012cdd9d1f87d1751070834e49d69d6
Zach Fogg <zach.fogg@gmail.com>
2014-02-03 19:34:58 -0500

    asd

--- sesh1.js
+++ sesh1.js
@@ -1,3 +1,8 @@
 var twitter = require('twitter');
 
-console.log(twitter);
+var twit = new twitter({
+    consumer_key:        process.env.CONSUMER_KEY,
+    consumer_secret:     process.env.CONSUMER_SECRET,
+    access_token_key:    process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
