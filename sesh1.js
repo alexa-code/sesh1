6f77faf15e884a9869363480eb665a58ff3e575d
Zach Fogg <zach.fogg@gmail.com>
2014-02-03 19:52:19 -0500

    stuff ;stuff ;))

--- sesh1.js
+++ sesh1.js
@@ -1,8 +1,29 @@
 var twitter = require('twitter');
+var util = require('util');
+
+
+console.log(process.env.CONSUMER_KEY);
+console.log(process.env.CONSUMER_SECRET)
+console.log(process.env.ACCESS_TOKEN_KEY)
+console.log(process.env.ACCESS_TOKEN_SECRET)
+
 
 var twit = new twitter({
     consumer_key:        process.env.CONSUMER_KEY,
     consumer_secret:     process.env.CONSUMER_SECRET,
     access_token_key:    process.env.ACCESS_TOKEN_KEY,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+
+twit
+
+.verifyCredentials(function(data) {
+    console.log("Authorized "+data.screen_name+" successfully!");
+})
+
+.search('nodejs OR #node', function(data) {
+    data.statuses.map(function (x) {
+        console.log(x.text);
+    });
 });
